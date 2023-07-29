6210cce59d0ff0775598f9cb4d803ba2eda6cb05
Nguyễn Hoàng Duy <hsbdu9@gmail.com>
2023-07-29 11:22:54 +0700

    123

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,14 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int id = 12312412;
+        String name ="Duy";
+        String address = "HN";
+        int age = 23;
+        double experience= 2;
+        String placeWork = "HN";
+        Employee employee = new Employee(id,name,address,age,experience,placeWork);
+        System.out.println(employee);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+    private int id;
+    private String name;
+    private String address;
+    private int age;
+    public Person(int id, String name, String address, int age) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.age = age;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public int getAge() {
+        return age;
+    }
 }
